16938e4b49e69f8f1bc679a779a3fdd9b1cdc362
MihCom9 <mihail.ya.mihaylov.2023@elsys-bg.org>
2025-12-09 00:12:12 +0200

    Created a basic spring backend

---
+++ Backend/demo/src/test/java/com/example/demo/ShopPricerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopPricerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
