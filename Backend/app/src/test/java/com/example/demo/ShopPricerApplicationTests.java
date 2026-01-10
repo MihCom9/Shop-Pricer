b796a2ce9e096985065ea8f61ff9f62fa31a10b3
MihCom9 <mihail.ya.mihaylov.2023@elsys-bg.org>
2026-01-10 17:45:00 +0200

    A fix to test errors with postgre, but not a final one

--- Backend/app/src/test/java/com/example/demo/ShopPricerApplicationTests.java
+++ Backend/app/src/test/java/com/example/demo/ShopPricerApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.demo;
 
+// import io.restassured.RestAssured;
+// import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 class ShopPricerApplicationTests {
-	
 	@Test
 	void contextLoads() {
 	}
