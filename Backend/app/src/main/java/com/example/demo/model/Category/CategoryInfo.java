ba3027a21ef10b43773f305975e98a4be5d1bd8c
MihCom9 <mihail.ya.mihaylov.2023@elsys-bg.org>
2026-04-30 16:04:24 +0300

    Added category model for controllers

---
+++ Backend/app/src/main/java/com/example/demo/model/Category/CategoryInfo.java
@@ -0,0 +1,22 @@
+package com.example.demo.model.Category;
+
+public class CategoryInfo {
+    private String name;
+    private String unitType;
+
+    public CategoryInfo(){}
+
+    public CategoryInfo(String name, String unitType) {
+        this.name = name;
+        this.unitType = unitType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUnitType() {
+        return unitType;
+    }
+
+}
